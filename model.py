40f231bfda02b8338cad3bbf8caf1125df3c91bc
planet nine <user@Planet-Nine.local>
2024-04-17 17:20:31 +0300

    adding tf.lite.model

--- model.py
+++ model.py
@@ -1,19 +1,21 @@
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-import keras
 
 IMG_SHAPE = 200
-model_path = './model/version_0p5.keras'
+model_path = './model/version_0p1.keras'
 objects = ['earth', 'jupiter', 'mars', 'mercury', 'moon', 'neptune', 'pluto', 'saturn', 'sun', 'uranus', 'venus']
-# model = keras.models.load_model(model_path)
 
-model = tf.keras.saving.load_model(model_path, custom_objects=None, compile=True, safe_mode=True)
+interpreter = tf.lite.Interpreter(model_path='./model/model_compressed.tflite')
+interpreter.allocate_tensors()
 
 def preprocessing_image(image_path):
     img = Image.open(image_path)
     img = img.resize((IMG_SHAPE, IMG_SHAPE))
-    img = np.array(img) / 255.0
+    img = np.array(img, dtype=np.float32) / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    # img = np.array(img) / 255.0
     return img
 
 def get_classes():
@@ -21,16 +23,23 @@ def get_classes():
 
 def predict_object(img_path):
     image = preprocessing_image(img_path)
-    predicted = model.predict(np.expand_dims(image, axis=0))
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], image)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    predicted = output_data[0]
 
     predicted_object = objects[np.argmax(predicted)]
     predicted_perc = np.max(predicted).astype('float') * 100
 
-    predicted[0][np.argmax(predicted)] = 0
+    predicted[np.argmax(predicted)] = 0
     second_predicted = objects[np.argmax(predicted)]
     second_perc = np.max(predicted).astype('float') * 100
 
-    predicted[0][np.argmax(predicted)] = 0
+    predicted[np.argmax(predicted)] = 0
     third_predicted = objects[np.argmax(predicted)]
     third_perc = np.max(predicted).astype('float') * 100
 
@@ -39,6 +48,6 @@ def predict_object(img_path):
         (second_predicted, second_perc),
         (third_predicted, third_perc)
     ]
-    print(predicted)
+    print(data)
     return data
 
